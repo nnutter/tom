c3effa0d342a9f9299dd978812b38a056c959668
Nathan Nutter <nnutter@duck.com>
2026-09-17 18:54:51 -0500

    Wire build-time main.version through to Cobra --version

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 	"github.com/nnutter/tom/cmd"
 )
 
+// version is the tom release version. Set it at build time with:
+//
+//	go build -ldflags "-X main.version=v1.2.3" .
+var version = "dev"
+
 func main() {
-	if err := cmd.Execute(); err != nil {
+	if err := cmd.Execute(version); err != nil {
 		os.Exit(1)
 	}
 }
